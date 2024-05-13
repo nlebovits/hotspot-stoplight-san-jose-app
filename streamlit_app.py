9c9df991b279255c4494d3660182752bf1dc8c3b
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-05-13 11:06:13 -0400

    get working with kmz data

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 
 import ee
 import geemap
+from geemap import geojson_to_ee
 import colorcet
 import requests
+import json
 from google.cloud import storage
+import tempfile
 import streamlit as st
 
 load_dotenv()
@@ -15,8 +18,6 @@ key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 os.environ["GDAL_DISABLE_READDIR_ON_OPEN"] = "YES"
 os.environ["CPL_VSIL_CURL_ALLOWED_EXTENSIONS"] = "tif"
 
-
-print(key_path)
 
 st.set_page_config(layout="wide")
 
@@ -59,6 +60,43 @@ clim = geemap.load_GeoTIFF("gs://hotspotstoplight-sanjose-ui/clim_top_cluster_id
 urb = geemap.load_GeoTIFF("gs://hotspotstoplight-sanjose-ui/urb_top_cluster_idx_cog.tif")
 urbex = geemap.load_GeoTIFF("gs://hotspotstoplight-sanjose-ui/urbex_top_cluster_idx_cog.tif")
 
+blob = bucket.blob('Priority_Zones_Bio_SiteVisit.kmz')
+
+import subprocess
+import zipfile
+import glob
+
+# Download the KMZ file to a temporary file
+with tempfile.NamedTemporaryFile(suffix='.kmz', delete=False) as temp_file:
+    blob.download_to_filename(temp_file.name)
+    kmz_path = temp_file.name
+
+# Unzip KMZ to extract the KML file
+kml_path = kmz_path.replace('.kmz', '.kml')
+with zipfile.ZipFile(kmz_path, 'r') as zip_ref:
+    zip_ref.extractall(os.path.dirname(kml_path))
+    # Assuming the main file is named 'doc.kml' inside the KMZ
+    kml_file = os.path.join(os.path.dirname(kml_path), 'doc.kml')
+
+# Convert KML to SHP using GDAL's ogr2ogr
+shp_path = kmz_path.replace('.kmz', '.shp')
+cmd = f'ogr2ogr -f "ESRI Shapefile" {shp_path} {kml_file}'
+subprocess.run(cmd, shell=True, check=True)
+
+# Convert SHP to Earth Engine object
+try:
+    bio_zones = geemap.shp_to_ee(shp_path)
+    print("Conversion to Earth Engine object successful.")
+except Exception as e:
+    print("Failed to convert SHP to Earth Engine object:", e)
+
+# Cleanup: remove temporary files
+os.remove(kmz_path)
+os.remove(kml_file)
+for file in glob.glob(shp_path.replace('.shp', '.*')):  # Remove all shapefile related files
+    os.remove(file)
+
+
 
 Map = geemap.Map()
 
@@ -91,6 +129,7 @@ Map.addLayer(bio, vizParamsBio, "bio")
 Map.addLayer(clim, vizParamsClim, "clim")
 Map.addLayer(urb, vizParamsUrb, "urban")
 Map.addLayer(urbex, vizParamsUrbex, "urbex")
+Map.addLayer(bio_zones, {}, "bio zones")
 # Map.addLayer(image, vizParamsProbability, "Flood Prob")
 Map.add_basemap('CartoDB.PositronOnlyLabels')
 Map.centerObject(bio, 11)
