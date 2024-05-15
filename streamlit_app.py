d5e3060fa2846ba3f5f5df674a16a7a221c4cb28
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-05-15 07:40:03 -0400

    switch to using geojsons stored on github

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import ee
 import geemap
+from geemap import geojson_to_ee
 import colorcet
 from google.cloud import storage
 import tempfile
@@ -9,17 +10,16 @@ import subprocess
 import zipfile
 import glob
 import streamlit as st
+import geopandas as gpd
 
-from utils.geoutils import load_geotiff, process_kmz_to_ee_feature
+from utils.geoutils import load_geotiff
 
 load_dotenv()
-
 
 cloud_project = os.getenv("GOOGLE_CLOUD_PROJECT_NAME")
 storage_client = storage.Client()
 bucket = storage_client.bucket("hotspotstoplight-sanjose-ui")
 ee.Initialize(project=cloud_project)
-
 
 st.set_page_config(layout="wide")
 st.sidebar.title("About")
@@ -35,7 +35,6 @@ st.markdown("""
     This app displays geospatial data for the Hotspot Stoplight trip to San Jose, Costa Rica in May of 2024. It is forked and modified from [Qiusheng Wu's work](https://github.com/giswqs/streamlit-multipage-template).
 """)
 
-
 vizParams = {
     "bio": {'min': 0, 'max': 1, 'palette': colorcet.rainbow},
     "clim": {'min': 0, 'max': 1, 'palette': colorcet.fire},
@@ -50,23 +49,31 @@ layers = {
     "urbex": load_geotiff("urbex")
 }
 
+@st.cache_data
+def read_data(url):
+    return gpd.read_file(url)
+
+site_urls = [
+    'https://github.com/HotspotStoplight/HotspotStoplight/raw/main/WebMap/geojson/Priority_Bio_SiteVisit.geojson',
+    'https://github.com/HotspotStoplight/HotspotStoplight/raw/main/WebMap/geojson/Priority_Clim_SiteVisit.geojson',
+    'https://github.com/HotspotStoplight/HotspotStoplight/raw/main/WebMap/geojson/Priority_Urb_SiteVisit.geojson',
+    'https://github.com/HotspotStoplight/HotspotStoplight/raw/main/WebMap/geojson/Priority_UrbEx_SiteVisit.geojson',
+    'https://github.com/HotspotStoplight/HotspotStoplight/raw/main/WebMap/geojson/Priority_Expansion_SiteVisit.geojson'
+]
 
 Map = geemap.Map()
 Map.add_basemap('Esri.WorldImagery')
+
+# Add GeoJSON layers
+for url in site_urls:
+    name = url.split('/')[-1].split('.')[0]
+    data = read_data(url)
+    ee_layer = geemap.geopandas_to_ee(data)
+    Map.addLayer(ee_layer, {}, name)
+
+# Add GeoTIFF layers
 for name, layer in layers.items():
     Map.addLayer(layer, vizParams[name], name)
-
-blob_names = [
-    'Priority_Zones_Clim_SiteVisit.kmz',
-    'Priority_Zones_Bio_SiteVisit.kmz',
-    'Priority_Zones_Urb_SiteVisit.kmz',
-    'Priority_Zones_UrbEx_SiteVisit.kmz',
-    'Priority_Zones_Expansion_SiteVisit.kmz'
-]
-for blob_name in blob_names:
-    ee_feature = process_kmz_to_ee_feature(bucket, blob_name)
-    if ee_feature:
-        Map.addLayer(ee_feature, {}, blob_name.split('.')[0])
 
 Map.add_basemap('CartoDB.PositronOnlyLabels')
 Map.centerObject(layers['bio'], 11)
